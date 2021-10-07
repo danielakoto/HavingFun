da282bc78d89fad8ed4821b98924474f4691823c
Daniel Akoto <sult4nbiz@gmail.com>
2021-10-07 09:53:51 -0400

    updating app

--- public/script.js
+++ public/script.js
@@ -1,3 +1,9 @@
+
+//await sleep function
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+let level = 1;
 const hf = document.getElementById("havingfun");
 function doS(x) {
     let rand = Math.floor(Math.random() * x);
@@ -14,3 +20,25 @@ function headsortails() {
         ht.innerHTML = "tails";
     console.log(rand + " " + ht.innerHTML);
 }
+let list = [];
+
+const sc = document.getElementById("somethingcool");
+async function somethingcool() {
+    for(let i = 0; i < 10; i++){
+        await sleep(500);
+        console.log("I was here");
+        let num = Math.floor(Math.random() * 101)
+        sc.innerHTML = num;
+        list[i] = num;
+    }
+    await sleep(500);
+    sc.innerHTML = "done";
+
+    list.forEach(e => {
+        console.log(e);
+    });
+
+}
+
+
+  
