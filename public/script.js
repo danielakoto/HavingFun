bb0f38fc1d6c24505c9a76f160c4e8da2444b569
daniel <sult4nbiz@gmail.com>
2024-08-08 00:47:02 -0400

    cahnges

--- public/script.js
+++ public/script.js
@@ -16,69 +16,66 @@ let amount = [11, 91, 891, 8991];
 let list = "";
 
 
-const sc = document.getElementById("somethingcool");
+const headerMessage = document.getElementById("header-message");
 const numbers = document.getElementById("numbers");
-const some = document.getElementById("some");
+const startButtonID = document.getElementById("start-button-id");
+const submitButtonID = document.getElementById("submit-button-id")
 const cAnswer = document.getElementById("correctOrNot");
 const cc = document.getElementById("check");
 //GAME START
-async function somethingcool() { 
+async function handleStartFunc() {
     cc.value = ""
     list = ""
-    some.style.display = "none";
+    startButtonID.style.display = "none";
+    submitButtonID.style.display = "inline-block"
     if(level >= 1 && level <= 5) {
         for(let i = 0; i < 3; i++){
                 await sleep(1000-(50*level));
                 let num = Math.floor(Math.random() * amount[0]);
-                sc.innerHTML = "-";
+                headerMessage.innerHTML = "-";
                 await sleep(50);
-                sc.innerHTML = num;
+                headerMessage.innerHTML = num;
                 list += num;
         }
     } else if(level >= 6 && level <= 10){
         for(let i = 0; i < 3; i++){
             await sleep(1000-(10*level));
             let num = Math.floor(Math.random() * amount[1])+10;
-            sc.innerHTML = "-";
+            headerMessage.innerHTML = "-";
             await sleep(50);
-            sc.innerHTML = num;
+            headerMessage.innerHTML = num;
             list += num;
         }
     } else if (level >= 11 && level <= 15) {
         for(let i = 0; i < 3; i++){
             await sleep(1000);
             let num = Math.floor(Math.random() * amount[2])+100;
-            sc.innerHTML = "-";
+            headerMessage.innerHTML = "-";
             await sleep(50);
-            sc.innerHTML = num;
+            headerMessage.innerHTML = num;
             list += num;
         }
     } else {
         for(let i = 0; i < 3; i++){
             await sleep(1000);
             let num = Math.floor(Math.random() * amount[3])+1000;
-            sc.innerHTML = "-";
+            headerMessage.innerHTML = "-";
             await sleep(50);
-            sc.innerHTML = num;
+            headerMessage.innerHTML = num;
             list += num;
         }
     }
 
     await sleep(500);
-    sc.innerHTML = "done";
+    headerMessage.innerHTML = "done";
 }
 
 
 //CHECKING ANSWER
 async function check() {
     
-    console.log(cc.value);
     let str = cc.value 
 
-    console.log(str)
-    console.log(list)
-    
-    console.log(list == str);
     if(list == str) {
         cAnswer.innerHTML = "";
         await sleep(100);
@@ -86,21 +83,20 @@ async function check() {
         await sleep(100);
         level++;
         getLevel();
-        sc.innerHTML = "Continue!";
-        some.innerHTML = "Next";
-        some.style.display = "inline-block";
-        cc.value = "";
+        headerMessage.innerHTML = "Continue!";
+        startButtonID.innerHTML = "Next";
     } else {
         level = 1;
         getLevel();
         cAnswer.innerHTML = "";
         await sleep(100);
-        cAnswer.innerHTML = "Not! <br> Numbers: " + list;
-        sc.innerHTML = "Try again";
-        some.innerHTML = "Start";
-        some.style.display = "inline-block";
-        cc.value = "";
+        cAnswer.innerHTML = "Wrong! <br> Numbers: " + list;
+        headerMessage.innerHTML = "Try again";
+        startButtonID.innerHTML = "Start";
     }
+    cc.value = "";
+    startButtonID.style.display = "inline-block";
+    submitButtonID.style.display = "none"
 }
 
 //Level getter
